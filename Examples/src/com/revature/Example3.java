2c6c5f66a61b545cad76340f5476ce7757cf6806
hyavgin <halidavgin@gmail.com>
2022-06-15 14:43:09 -0500

    new

--- Examples/src/com/revature/Example3.java
+++ Examples/src/com/revature/Example3.java
@@ -2,28 +2,58 @@ package com.revature;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+
 
 public class Example3 {
 	
 	public static void main(String[] args) {
-		String S = new String("abacabcxyhkl");
+		String S = new String("ebgnhamdhnuxbvzlufpkksnbvdsssjdwkkjumxxtntsooraiyrsllimgnhafq");
 		char[] s1 = S.toCharArray();
-		Arrays.sort(s1);
-		char remove = S.charAt(0);
-		System.out.println(remove);
+		//Arrays.sort(s1);
+		//String Ss = new String(s1);
+		//System.out.println(s1);
 		String Result = "-404";
-		for(int i =0;i<S.length();i++) {
-			for(int k =i+1;k<S.length();k++) {
-				if(S.charAt(i)==S.charAt(k)) {
-					int index=S.charAt(k);
-					//s1.delete(k);
-					//s1[k]=;
-					
+		//char a = S.charAt(5);
+		//System.out.println(a);
+		String r = "";
+		
+		//StringBuilder str = new StringBuilder(S);
+		
+		String[] SArray = null;
+		SArray = S.split("");  
+		ArrayList<String> deleted =new ArrayList<String>();
+		//converting using String.split() method with whitespace as a delimiter  
+		deleted.add(SArray[0]);
+		System.out.println(deleted);
+		for(int i=1;i<SArray.length;i++) {
+			if(deleted.contains(SArray[i])) {
+				//System.out.println("Contains:"+SArray[i]);
+				}else {
+					deleted.add(SArray[i]);
 				}
-				
-			}
+			
+		}String listString = "";
+
+		for (String s : deleted)
+		{
+			listString += s;
 		}
+
+		System.out.println(listString);
+		//System.out.print(deleted);
+		
+		
+		/*for(int i =1;i<S.length();i++) {
+			
+			if(s1[i]==s1[i-1] && s1[i]==s1[i-2] ) {
+					char newresult=s1[i];
+					System.out.print(newresult);
+
+			}else 
+			
+		}*/
 
 		}		
 		
